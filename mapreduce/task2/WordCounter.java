2c8fd477a5b883f1c60cf22dffdd1e4483484c43
Mingyang Li <48164276+TomLee13@users.noreply.github.com>
2019-09-19 16:07:39 -0400

    Add files via upload

---
+++ mapreduce/task2/WordCounter.java
@@ -0,0 +1,88 @@
+// ======================= WordCount.java ==========================================
+package org.myorg;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.util.*;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+
+public class WordCounter extends Configured implements Tool {
+
+        public static class WordCounterMap extends Mapper<LongWritable, Text, Text, IntWritable>
+        {
+                private final static IntWritable one = new IntWritable(1);
+                private Text word = new Text();
+                
+                @Override
+                public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
+                {
+                        String line = value.toString();
+                        StringTokenizer tokenizer = new StringTokenizer(line);
+                        while(tokenizer.hasMoreTokens())
+                        {
+                                String next = tokenizer.nextToken();
+				System.out.println("next");
+				word.set("count");
+                                context.write(word, one);
+                        }
+                }
+        }
+        
+        public static class WordCounterReducer extends Reducer<Text, IntWritable, Text, IntWritable>
+        {
+                public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+                {
+                        int sum = 0;
+                        for(IntWritable value: values)
+                        {
+                                sum += value.get();
+                        }
+                        context.write(key, new IntWritable(sum));
+                }
+                
+        }
+        
+        public int run(String[] args) throws Exception  {
+               
+                Job job = new Job(getConf());
+                job.setJarByClass(WordCounter.class);
+                job.setJobName("wordcounter");
+                
+                job.setOutputKeyClass(Text.class);
+                job.setOutputValueClass(IntWritable.class);
+                
+                job.setMapperClass(WordCounterMap.class);
+                job.setCombinerClass(WordCounterReducer.class);
+                job.setReducerClass(WordCounterReducer.class);
+                
+                
+                job.setInputFormatClass(TextInputFormat.class);
+                job.setOutputFormatClass(TextOutputFormat.class);
+                
+                
+                FileInputFormat.setInputPaths(job, new Path(args[0]));
+                FileOutputFormat.setOutputPath(job, new Path(args[1]));
+                
+                boolean success = job.waitForCompletion(true);
+                return success ? 0: 1;
+        }
+        
+       
+        public static void main(String[] args) throws Exception {
+                // TODO Auto-generated method stub
+                int result = ToolRunner.run(new WordCounter(), args);
+                System.exit(result);
+        }
+       
+} 
